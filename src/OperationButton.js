01aaa2efb9ca19955d353902ad8cfdcfc46773a2
FernandoSaru <105390521+FernandoSaru@users.noreply.github.com>
2022-07-25 23:10:05 -0300

    Project Start + Buttons + CSS +display layout +background

---
+++ src/OperationButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App"
+
+export default function OperationButton({ dispatch, operation }) {
+    return (
+        <button
+            onClick={() =>
+                dispatch({ type: ACTIONS.SELECT_OPERATION, payload: { operation } })
+            }
+        >
+            {operation}
+        </button>
+    )
+}
